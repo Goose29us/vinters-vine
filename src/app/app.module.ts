bad0164a8369d5a1e1a5e0cbc46402294c73a48d
Goose29us <jguse@new.rr.com>
2018-12-23 21:52:25 -0600

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,10 +10,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from './material.module';
+import { IonicStorageModule } from '@ionic/storage';
+import 'rxjs/operators';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule, MaterialModule],
   providers: [
     StatusBar,
     SplashScreen,
